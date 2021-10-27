e2d4979f0a43ea2b64c0e73ac26dcc1afe2a27a6
Mikhail <mikko.deriabin@gmail.com>
2021-10-27 14:25:43 +0300

    tasks 7.1, 7.2, 7.3 completed

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -1,8 +1,12 @@
 // tasks.js
 // This script manages a to-do list.
 
+const removeDuplicatesBtn = document.querySelector("#removeDuplicates")
+
 // Need a global variable:
-var tasks = []; 
+let tasks = [];
+
+removeDuplicatesBtn.addEventListener("click", removeDuplicates);
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
@@ -10,27 +14,18 @@ function addTask() {
     'use strict';
 
     // Get the task:
-    var task = document.getElementById('task');
+    const task = document.getElementById('task');
 
     // Reference to where the output goes:
-    var output = document.getElementById('output');
+    const output = document.getElementById('output');
     
     // For the output:
-    var message = '';
+    let message = '';
 
     if (task.value) {
-    
         // Add the item to the array:
         tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
-        
+        updateTaskList();
     } // End of task.value IF.
 
     // Return false to prevent submission:
@@ -44,3 +39,26 @@ function init() {
     document.getElementById('theForm').onsubmit = addTask;
 } // End of init() function.
 window.onload = init;
+
+function removeDuplicates(){
+    let result = [];
+    if(tasks.length > 0){
+        for(let i=0; i <tasks.length-1; i++){
+            if(!result.includes(tasks[i]))
+                result.push(tasks[i]);
+        }
+        tasks = result;
+        updateTaskList();
+    }
+}
+
+function updateTaskList(){
+    const output = document.getElementById('output');
+
+        let message = '<h2>To-Do</h2><ol>';
+        for (let i = 0, count = tasks.length; i < count; i++) {
+            message += '<li>' + tasks[i] + '</li>';
+        }
+        message += '</ol>';
+        output.innerHTML = message;
+}
