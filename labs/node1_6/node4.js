5cab3fa62b7f3d9707f8cb33f1a89dfe70e6e4ae
Mikhail <mikko.deriabin@gmail.com>
2021-11-19 13:50:11 +0200

    tasks 1-6 node done

--- labs/node1_6/node4.js
+++ labs/node1_6/node4.js
@@ -2,43 +2,49 @@ const mysql = require('mysql');
 const util = require("util");
 const express = require('express');
 
-const app = express();
-let queryResult = "";
-
-const con = mysql.createConnection({
+const dbConfig = {
     host: "localhost",
     user: "user",
     password: "user",
     database: "example_db"
-});
+};
 
-const query = util.promisify(con.query).bind(con);
+const app = express();
 
-(async () => {
+const makeQuery = async (sqlQuery) => {
+    const con = mysql.createConnection(dbConfig);
+    const query = util.promisify(con.query).bind(con);
+
     try {
-        const result = await query('SELECT * FROM event');
-        console.log(result);
+        return await query(sqlQuery);
+    } catch(err){
+        console.log("Error during getting data from DB:");
+        console.log(err);
     } finally {
         con.end();
     }
-})()
+}
 
-/*
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM event", function (err, result) {
-        if (err) throw err;
-        queryResult = result;
+const someQuery = 'SELECT * FROM event';
+
+app.get('/', (req, res) => {
+    makeQuery(someQuery).then((result) => {
+        console.log(result);
+        res.send(result);
+    });
+    console.log("This line will be first");
+});
+
+const anotherQuery = "SELECT * FROM event_date";
+app.get("/date", (req, res) => {
+    makeQuery(anotherQuery).then((result) => {
+        res.send(result);
     });
 });
-app.get('/', function (req, res) {
-    res.send(queryResult);
-})
 
-const server = app.listen(8081, function () {
-    const host = server.address().address
-    const port = server.address().port
+const server = app.listen(8081, () => {
+    const host = server.address().address;
+    const port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port)
-})
-*/
+});
